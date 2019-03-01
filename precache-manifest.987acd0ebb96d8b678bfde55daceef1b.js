43a1adb5ae37aecd04df9fd24740e6fdbb8823f2
chaixb <1481569358@qq.com>
2019-03-01 22:14:47 +0800

    Updates

---
+++ precache-manifest.987acd0ebb96d8b678bfde55daceef1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7b9d54519cd133dd741",
+    "url": "/jsondiffpatch-by-url/static/css/main.b62b3c3c.chunk.css"
+  },
+  {
+    "revision": "b7b9d54519cd133dd741",
+    "url": "/jsondiffpatch-by-url/static/js/main.b7b9d545.chunk.js"
+  },
+  {
+    "revision": "02e363918283a384eaae",
+    "url": "/jsondiffpatch-by-url/static/js/runtime~main.02e36391.js"
+  },
+  {
+    "revision": "7155950ca962c44a10be",
+    "url": "/jsondiffpatch-by-url/static/css/2.fca237a0.chunk.css"
+  },
+  {
+    "revision": "7155950ca962c44a10be",
+    "url": "/jsondiffpatch-by-url/static/js/2.7155950c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jsondiffpatch-by-url/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9284113e7560564770afd9b58ec1a372",
+    "url": "/jsondiffpatch-by-url/index.html"
+  }
+];
